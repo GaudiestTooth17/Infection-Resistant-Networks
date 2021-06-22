4e2445a51f4166d6756669fdf8a6e26a681900dd
GaudiestTooth17 <alex.r.anthon@gmail.com>
2021-06-22 14:11:26 -0600

    Fixed the bug. It turns out it was actually with Visualize.

--- sim_dynamic.py
+++ sim_dynamic.py
@@ -1,4 +1,5 @@
 from typing import Callable, List, Optional, Tuple
+import time
 import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
@@ -67,26 +68,26 @@ def next_sir(old_sir: np.ndarray, M: np.ndarray, disease: Disease, rand) -> Tupl
     the new. The first dimension of sir is state. The second dimension is node.
     """
 
-    seir = np.copy(old_sir)
+    sir = np.copy(old_sir)
     N = M.shape[0]
     probs = rand.random(N)
 
     # infectious to recovered
-    to_r_filter = seir[1] > disease.days_infectious
-    seir[2, to_r_filter] = -1
-    seir[1, to_r_filter] = 0
+    to_r_filter = sir[1] > disease.days_infectious
+    sir[2, to_r_filter] = -1
+    sir[1, to_r_filter] = 0
 
     # susceptible to infectious
-    i_filter = seir[1] > 0
-    to_i_probs = (1 - np.prod(1 - (M * disease.trans_prob)[i_filter], axis=1)).reshape((N,))
-    to_i_filter = (seir[0] > 0) & (probs < to_i_probs)
-    seir[1, to_i_filter] = -1
-    seir[0, to_i_filter] = 0
+    i_filter = sir[1] > 0
+    to_i_probs = 1 - np.prod(1 - (M * disease.trans_prob)[i_filter], axis=0)
+    to_i_filter = (sir[0] > 0) & (probs < to_i_probs)
+    sir[1, to_i_filter] = -1
+    sir[0, to_i_filter] = 0
 
-    seir[seir > 0] += 1
-    seir[seir < 0] = 1
+    sir[sir > 0] += 1
+    sir[sir < 0] = 1
 
-    return seir, to_r_filter.any() or to_i_filter.any()
+    return sir, to_r_filter.any() or to_i_filter.any()
 
 
 def no_update(D: np.ndarray, M: np.ndarray, time_step: int, sir: np.ndarray) -> np.ndarray:
@@ -107,18 +108,19 @@ class Visualize:
         self._state_to_color = {0: 'blue', 1: 'green', 2: 'grey'}
 
     def __call__(self, G: nx.Graph, sir: np.ndarray) -> None:
-        node_to_state = [(node, 0) for node in sir[0, sir[0] > 0]]\
-            + [(node, 1) for node in sir[1, sir[1] > 0]] + [(node, 2) for node in sir[2, sir[2] > 0]]  # type: ignore
-        node_to_state.sort(key=lambda x: x[0])
-        node_colors = tuple(self._state_to_color[ns[1]] for ns in node_to_state)
+        node_colors = np.empty(len(G), dtype=np.object_)
+        for state in range(sir.shape[0]):
+            for node in range(sir.shape[1]):
+                if sir[state, node] > 0:
+                    node_colors[node] = self._state_to_color[state]
         plt.clf()
-        nx.draw_networkx(G, pos=self._layout, with_labels=False, node_color=node_colors, node_size=50)
-        # plt.pause(.2)  # type: ignore
-        plt.show()
+        nx.draw_networkx(G, pos=self._layout, with_labels=True, node_color=node_colors, node_size=50)
+        plt.pause(.2)  # type: ignore
 
 
 if __name__ == '__main__':
-    M, layout = read_network_file('networks/annealed-medium-diameter.txt')
+    # M, layout = read_network_file('networks/annealed-medium-diameter.txt')
+    M, layout = read_network_file('networks/cavemen-10-10.txt')
     disease = Disease(4, .25)
     sir0 = np.zeros((3, M.shape[0]), dtype=np.int64)
     sir0[0] = 1
@@ -126,3 +128,4 @@ if __name__ == '__main__':
     sir0[0, 0] = 0
 
     simulate(M, sir0, disease, remove_dead_agents, 100, layout)
+    input()
