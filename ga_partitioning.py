f4b78797c10003fb901c93470792e566fbaa1163
GaudiestTooth17 <alex.r.anthon@gmail.com>
2021-06-25 12:11:49 -0600

    Finished implementing Chakraborty-Sato. It runs really slowly, so I haven't tested it much.

--- ga_partitioning.py
+++ ga_partitioning.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
-from analyzer import all_same, visualize_graph
+from analyzer import visualize_graph
 from hcmioptim import ga
 import networkx as nx
 import numpy as np
+import matplotlib.pyplot as plt
 from typing import Dict, Sequence, Tuple
 import sys
 import itertools as it
@@ -14,28 +15,27 @@ from fileio import read_network_file
 
 
 def main():
-    """
-    Initial testing indicates that this is a REALLY good community detection algorithm!!
-    UPDATE: Further testing showed it to be lack luster.
-    It didn't do great on the elitist network, but perhaps with more time it could get better results.
-    It was really crappy on Watts-Strogatz.
-    """
     if len(sys.argv) < 2:
         print(f'Usage: {sys.argv[0]} <network>')
         return
 
     M, layout = read_network_file(sys.argv[1])
     G = nx.Graph(M)
-    rand = np.random.default_rng()
-    objective = PartitioningObjective(G)
-    optimizer = ga.GAOptimizer(objective,
-                               NextEdgesToRm(rand),
-                               new_to_rm_pop(len(G.edges), 20, rand),
-                               True, 1)  # it's like 4x faster with only one core
+    rand = np.random.default_rng(0)
+    # objective = PartitioningObjective(G)
+    # optimizer = ga.GAOptimizer(objective,
+    #                            NextEdgesToRm(rand),
+    #                            new_to_rm_pop(len(G.edges), 20, rand),
+    #                            True, 1)  # it's like 4x faster with only one core
+    optimizer = ga.GAOptimizer(ChakrabortySatoObjective(G),
+                               NextChakrabortySatoGen(rand, G),
+                               new_chakraborty_sato_pop(rand, G, 20),
+                               True, 1)
 
     n_steps = 1000
     pbar = tqdm(range(n_steps))
     costs = np.zeros(n_steps)
+    diversities = np.zeros(n_steps)
     global_best: Tuple = None  # type: ignore
     for step in pbar:
         cost_to_encoding = optimizer.step()
@@ -43,14 +43,17 @@ def main():
         if global_best is None or local_best[0] < global_best[0]:
             global_best = local_best
         costs[step] = local_best[0]
-        pbar.set_description('Cost: {}'.format(local_best[0]))
+        diversities[step] = len({tuple(ce[1]) for ce in cost_to_encoding}) / len(cost_to_encoding)
+        pbar.set_description('Cost: {:.3f}'.format(local_best[0]))
 
-    partitioned = objective.partition(global_best[1])
+    # partitioned = objective.partition(global_best[1])
+    partitioned = nx.to_numpy_array(chakraborty_sato_partition(G, global_best[1]))
     print('Cost:', global_best[0])
 
-    visualize_graph(partitioned, layout, 'Partitioned via GA',
-                    False, all_same, False)
-    input('Press <enter> to exit.')
+    plt.title('Diversity')
+    plt.plot(diversities)
+    plt.figure()
+    visualize_graph(partitioned, layout, 'Partitioned via CS GA')
 
 
 class PartitioningObjective:
@@ -109,6 +112,7 @@ def new_to_rm_pop(edges: int, size: int, rand) -> Tuple[np.ndarray, ...]:
 
 class ChakrabortySatoObjective:
     def __init__(self, G: nx.Graph) -> None:
+        """The objective function for community detection as described by Chakraborty and Sato."""
         self._E = len(G.edges)
         self._M = nx.to_numpy_array(G)
         self._nodes = tuple(G.nodes)
@@ -118,10 +122,12 @@ class ChakrabortySatoObjective:
         """
         Rate the encoding based on how well it clusters.
 
-        Encodings encode a list of N edges. One end of the edge is the location in the array. The other is the value.
+        Encodings encode a list of N edges. One end of the edge is the location in the array.
+        The other is the value.
         """
         cluster_forest = nx.Graph(tuple((u, v) for u, v in enumerate(encoding)))
-        clusters = nx.connected_components(cluster_forest)
+        clusters = tuple(nx.connected_components(cluster_forest))
+        # import pdb; pdb.set_trace()
 
         def lookup_cluster(node):
             for cluster in clusters:
@@ -130,13 +136,62 @@ class ChakrabortySatoObjective:
             raise Exception(f'Could not find cluster for {node}.')
         node_to_cluster = {node: lookup_cluster(node) for node in self._nodes}
 
-        return (1/(2*self._E)) * np.sum(tuple((self._M[i, j] - (self._k[i]*self._k[j])/(2*self._E))
-                                        * self._delta(node_to_cluster[i], node_to_cluster[j])
-                                              for i, j in it.product(self._nodes, self._nodes)))
+        return -(1/(2*self._E)) * np.sum(tuple((self._M[i, j] - (self._k[i]*self._k[j])/(2*self._E))
+                                         * self._delta(node_to_cluster[i], node_to_cluster[j])
+                                               for i, j in it.product(self._nodes, self._nodes)))
 
     @staticmethod
     def _delta(cluster_i, cluster_j) -> int:
         return cluster_i == cluster_j
+
+
+class NextChakrabortySatoGen:
+    def __init__(self, rand, G: nx.Graph) -> None:
+        self._rand = rand
+        self._neighbors = {node: np.array(tuple(nx.neighbors(G, node))) for node in G}
+
+    def __call__(self, rated_pop: Sequence[Tuple[Number, np.ndarray]]) -> Tuple[np.ndarray, ...]:
+        couples = ga.tournament_selection(rated_pop)
+        offspring = (ga.single_point_crossover(*couple) for couple in couples)
+        children = tuple(child for pair in offspring for child in pair)
+
+        for i, j in it.product(range(len(children)), range(len(children[0]))):
+            if self._rand.random() < .001:
+                children[i][j] = self._rand.choice(self._neighbors[j])
+
+        return children
+
+
+def new_chakraborty_sato_pop(rand, G: nx.Graph, size: int) -> Tuple[np.ndarray, ...]:
+    neighbors = {node: tuple(nx.neighbors(G, node)) for node in G}
+
+    def make_encoding():
+        encoding = np.zeros(len(G), dtype=np.int64)
+        for i in range(encoding.shape[0]):
+            encoding[i] = rand.choice(neighbors[i])
+        return encoding
+    return tuple(make_encoding() for _ in range(size))
+
+
+def chakraborty_sato_partition(G: nx.Graph, encoding: np.ndarray) -> nx.Graph:
+    node_to_cluster = {}
+    current_cluster = 0
+    for u, v in enumerate(encoding):
+        u_registered = u in node_to_cluster
+        v_registered = v in node_to_cluster
+        if not u_registered and not v_registered:
+            node_to_cluster[u] = current_cluster
+            node_to_cluster[v] = current_cluster
+            current_cluster += 1
+        elif not u_registered:
+            node_to_cluster[u] = node_to_cluster[v]
+        elif not v_registered:
+            node_to_cluster[v] = node_to_cluster[u]
+
+    edges_to_remove = filter(lambda edge: node_to_cluster[edge[0]] != node_to_cluster[edge[1]], G.edges)
+    partitioned = nx.Graph(G)
+    partitioned.remove_edges_from(edges_to_remove)
+    return partitioned
 
 
 if __name__ == '__main__':
