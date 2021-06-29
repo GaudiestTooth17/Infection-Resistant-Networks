c842b2c64088294109303bddd7d20ec604e03670
Alexander Anthon <alex.r.anthon@gmail.com>
2021-06-29 12:38:31 -0600

    Switched to using the spring layout.

--- addlayout.py
+++ addlayout.py
@@ -3,6 +3,7 @@
 from fileio import output_network, read_network_file, get_network_name
 import networkx as nx
 import sys
+import time
 
 
 def main(argv):
@@ -12,13 +13,13 @@ def main(argv):
 
     M, layout = read_network_file(argv[1])
     name = get_network_name(argv[1])
-    if layout is not None:
-        print('Network already has a layout.')
-        return
 
     G = nx.Graph(M)
-    layout = nx.kamada_kawai_layout(G)
+    start_time = time.time()
+    # layout = nx.kamada_kawai_layout(G)
+    layout = nx.spring_layout(G, iterations=50, pos=layout)
     output_network(G, name+'-with-layout', layout)
+    print(f'Done ({time.time()-start_time}).')
 
 
 if __name__ == '__main__':
