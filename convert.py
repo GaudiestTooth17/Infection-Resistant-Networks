3c198b54c94aa8a1587f268628bdec526481f22a
Alexander Anthon <alex.r.anthon@gmail.com>
2021-07-02 10:21:15 -0600

    Added better analysis.

--- convert.py
+++ convert.py
@@ -1,7 +1,8 @@
 """Converts from the old network format to the new one."""
 
 import sys
-from partitioning import label_partition, intercommunity_edges_to_communities
+from partitioning import (girvan_newman_partition, intercommunity_edges_to_communities,
+                          label_partition)
 from fileio import old_read_network_file, get_network_name, write_network, read_network
 from analyzer import (visualize_graph, COLORS, make_meta_community_network,
                       make_meta_community_layout)
@@ -11,15 +12,9 @@ import matplotlib.pyplot as plt
 
 """
 networks to work on
-annealed-short-diameter: Edit this manually.
 barabasi-albert-500-3: This one might not be salvagable
-example-cgg: make sure this has the correct number of communities.
-cavemen-50-10: It doesn't look like GN worked. You should double check.
-cgg-520: I should just update the function to output communities.
-connected-community-100-10: see cgg-520
-evolved: I should just get rid of this entirely.
-spatial-network: I just need to verify the number of communities.
-watts-strogatz-500-4-.1: It looks like this was partitioned into 10
+cavemen-50-10: It doesn't look like GN worked. I should double check.
+connected-community-100-10: I should just update the function to output communities.
 """
 
 
@@ -34,7 +29,8 @@ def main():
         return
 
     G = nx.Graph(M)
-    interedges = label_partition(G, int(sys.argv[2]))
+    # interedges = label_partition(G, int(sys.argv[2]))
+    interedges = girvan_newman_partition(G, int(sys.argv[2]))
     communities = intercommunity_edges_to_communities(G, interedges)
 
     new_name = get_network_name(sys.argv[1])+'-new-format'
@@ -55,10 +51,13 @@ def main():
     meta_network, meta_ns, meta_ew = make_meta_community_network(interedges, partitioned)
     meta_layout = make_meta_community_layout(meta_network, layout)
 
+    print(f'There are {len(meta_network)} communities.')
     node_color = [COLORS[i] for i in communities.values()]
     visualize_graph(G, layout, new_name, node_color=node_color, block=False)  # type: ignore
     plt.figure()
-    visualize_graph(meta_network, meta_layout, new_name+'meta community',
+    plt.hist([len(comp) for comp in nx.connected_components(partitioned)], bins=None)
+    plt.figure()
+    visualize_graph(meta_network, meta_layout, new_name+' meta community',
                     node_size=meta_ns, edge_width_func=lambda G: meta_ew)
 
 
