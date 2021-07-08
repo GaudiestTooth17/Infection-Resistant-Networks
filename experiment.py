23fac3c3515340ead5e63e244a51e95442db174a
Alexander Anthon <alex.r.anthon@gmail.com>
2021-07-08 13:59:59 -0600

    Runs in parallel now.

--- experiment.py
+++ experiment.py
@@ -6,19 +6,22 @@ from customtypes import Number
 import numpy as np
 import networkx as nx
 from dataclasses import dataclass
-from typing import Sequence
+from typing import Any, Sequence, Tuple
 import os
 import csv
 from abc import ABC, abstractmethod
 import networkgen
 from tqdm import tqdm
+import matplotlib.pyplot as plt
+from multiprocessing import Pool
+import time
 
 
 @dataclass
 class MassDiseaseTestingResult:
     network_class: str
     """A string describing what type of network the simulations were run on."""
-    trial_to_results: Sequence[Sequence[int]]
+    trial_to_results: Sequence[Number]
     """Each result is actually a collection of results from one instance of the  network class."""
     trial_to_proportion_flickering_edges: Sequence[float]
     """The trial should line up with the trial in trial_to_results."""
@@ -33,9 +36,15 @@ class MassDiseaseTestingResult:
 
         with open(self.network_class+'.csv', 'w', newline='') as file:
             writer = csv.writer(file, dialect=csv.excel)
-            writer.writerows(self.trial_to_results)
-            writer.writerow([])
+            writer.writerow(['Results'])
+            writer.writerow(self.trial_to_results)
+            writer.writerow(['proportion flickering edges'])
             writer.writerow(self.trial_to_proportion_flickering_edges)
+
+        plt.figure()
+        plt.title(f'Results for {self.network_class}')
+        plt.boxplot(self.trial_to_results, notch=False)
+        plt.savefig(f'Results for {self.network_class}.png', format='png')
 
 
 class ConnectedCommunityConfiguration(ABC):
@@ -80,36 +89,57 @@ class PoissonConfiguration(ConnectedCommunityConfiguration):
         return outer_degrees
 
 
+def run_poisson_trial(args: Tuple[PoissonConfiguration, Disease, Any]) -> Tuple[float, float]:
+    """
+    args: (PoissonConfiguration to use,
+           disease to use)
+    """
+    sim_len = 200
+    sims_per_trial = 250
+    configuration, disease, rand = args
+
+    inner_degrees = configuration.make_inner_degrees()
+    outer_degrees = configuration.make_outer_degrees()
+    G, communities = networkgen.make_connected_community_network(inner_degrees, outer_degrees,
+                                                                 rand)
+    to_flicker = {(u, v) for u, v in G.edges if communities[u] != communities[v]}
+    proportion_flickering = len(to_flicker) / len(G.edges)
+    M = nx.to_numpy_array(G)
+
+    behavior = FlickerBehavior(M, to_flicker, (True, False), "Probs don't change this")
+    avg_sus = np.mean([np.sum(simulate(M,
+                                       make_starting_sir(len(M), 1),
+                                       disease, behavior, sim_len, None)[-1][0] > 0)
+                       for _ in range(sims_per_trial)])
+
+    return proportion_flickering, avg_sus
+
+
 def main():
+    start_time = time.time()
     N_comm = 10  # agents per community
     num_communities = 50  # number of communities
     num_trials = 1000
-    sims_per_trial = 1000
-    sim_len = 200
     rand = np.random.default_rng(420)
     configuration = PoissonConfiguration(f'Poisson: lam=10, {num_communities} comms, {N_comm} big',
                                          rand, 10, 10, num_communities, N_comm)
     disease = Disease(4, .2)
 
-    trial_to_results: Sequence[Sequence[int]] = []
-    trial_to_flickering_edges: Sequence[float] = []
-    for _ in tqdm(range(num_trials)):
-        inner_degrees = configuration.make_inner_degrees()
-        outer_degrees = configuration.make_outer_degrees()
-        G, communities = networkgen.make_connected_community_network(inner_degrees, outer_degrees, rand)
-        to_flicker = {(u, v) for u, v in G.edges if communities[u] != communities[v]}
-        trial_to_flickering_edges.append(len(to_flicker)/len(G.edges))
-        M = nx.to_numpy_array(G)
-        behavior = FlickerBehavior(M, to_flicker, (True, False),
-                                   "Probs don't change this")
-        trial_to_results.append([np.sum(simulate(M, make_starting_sir(len(M), 1),
-                                                 disease, behavior,
-                                                 sim_len, None)[-1][0])
-                                 for _ in range(sims_per_trial)])
-    experiment_results = MassDiseaseTestingResult(configuration.name, trial_to_results,
+    with Pool(10) as p:
+        results = p.map(run_poisson_trial, [(configuration, disease, rand)
+                                            for _ in range(num_trials)],
+                        num_trials//10)
+    trial_to_flickering_edges, trial_to_avg_sus = zip(*results)
+    experiment_results = MassDiseaseTestingResult(configuration.name, trial_to_avg_sus,
                                                   trial_to_flickering_edges)
     experiment_results.save('results')
+    print(f'Finished ({time.time()-start_time} s).')
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('\nGood bye.')
+    except EOFError:
+        print('\nGood bye.')
