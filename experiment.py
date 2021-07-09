f2e4017e0c45564837eee660840718c5bf20cc1e
Alexander Anthon <alex.r.anthon@gmail.com>
2021-07-09 14:13:03 -0600

    Added more experiment types.

--- experiment.py
+++ experiment.py
@@ -1,12 +1,13 @@
 """
 This file is for running experiments and reporting on them.
 """
+from tqdm.std import tqdm
 from sim_dynamic import Disease, FlickerBehavior, make_starting_sir, simulate
-from customtypes import ExperimentResults, Number
+from customtypes import Agent, Number
 import numpy as np
 import networkx as nx
 from dataclasses import dataclass
-from typing import Any, Sequence, Tuple
+from typing import Any, Dict, Sequence, Tuple
 import os
 import csv
 from abc import ABC, abstractmethod
@@ -95,13 +96,54 @@ class PoissonConfiguration(ConnectedCommunityConfiguration):
         return outer_degrees
 
 
-def run_poisson_trial(args: Tuple[PoissonConfiguration, Disease, Any]) -> Tuple[float, float]:
+class UniformConfiguration(ConnectedCommunityConfiguration):
+    def __init__(self, rand,
+                 inner_bounds: Tuple[int, int], outer_bounds: Tuple[int, int],
+                 N_comm: int, num_communities: int) -> None:
+        """
+        An object that stores the configuration for a connected community network.
+
+        inner_bounds: The (lower, upper) bounds for the intracommunity degree distribution
+        outer_bounds: The (lower, upper) bounds for the intercommunity degree distribution
+        N_comm: The number of nodes in a community
+        num_communities: The number of communities
+        """
+        self._name = f'UniformConfig(ib={inner_bounds}, ob={outer_bounds}'\
+                     f'N_comm={N_comm}, num_comms = {num_communities})'
+        self._rand = rand
+        self._inner_bounds = inner_bounds
+        self._outer_bounds = outer_bounds
+        self._N_comm = N_comm
+        self._num_communities = num_communities
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    def make_inner_degrees(self) -> np.ndarray:
+        inner_degrees = self._rand.integers(self._inner_bounds[0], self._inner_bounds[1],
+                                            self._N_comm)
+        if np.sum(inner_degrees) % 2 == 1:
+            inner_degrees[np.argmin(inner_degrees)] += 1
+        return inner_degrees
+
+    def make_outer_degrees(self) -> np.ndarray:
+        outer_degrees = self._rand.integers(self._outer_bounds[0], self._outer_bounds[1],
+                                            self._num_communities)
+        if np.sum(outer_degrees) % 2 == 1:
+            outer_degrees[np.argmin(outer_degrees)] += 1
+        return outer_degrees
+
+
+def run_connected_community_trial(args: Tuple[ConnectedCommunityConfiguration, Disease, Any])\
+        -> Tuple[float, float]:
     """
     args: (PoissonConfiguration to use,
-           disease to use)
+           disease to use,
+           default_rng instance)
     """
     sim_len = 200
-    sims_per_trial = 250
+    sims_per_trial = 150
     configuration, disease, rand = args
 
     inner_degrees = configuration.make_inner_degrees()
@@ -146,21 +188,74 @@ def run_agent_generated_trial(args: Tuple[Disease, abg.Behavior, int, Any]) -> T
     return proportion_flickering, avg_sus
 
 
+def run_social_circles_trial(args: Tuple[Dict[Agent, int],
+                                         Tuple[int, int],
+                                         Disease,
+                                         Any]) -> Tuple[float, float]:
+    agent_to_quantity, grid_dims, disease, rand = args
+    sim_len = 200
+    sims_per_trial = 150
+
+    sc_results = None
+    while sc_results is None:
+        sc_results = networkgen.make_social_circles_network(agent_to_quantity, grid_dims, rand=rand)
+    G, _, _ = sc_results
+
+    to_flicker = partitioning.fluidc_partition(G, 25)
+    proportion_flickering = len(to_flicker) / len(G.edges)
+    M = nx.to_numpy_array(G)
+
+    network_behavior = FlickerBehavior(M, to_flicker, (True, False), "Probs don't change this")
+    avg_sus = np.mean([np.sum(simulate(M, make_starting_sir(len(M), 1),
+                                       disease, network_behavior, sim_len, None, rand)[-1][0] > 0)
+                       for _ in range(sims_per_trial)])
+
+    return proportion_flickering, avg_sus
+
+
 def poisson_entry_point():
+    """Run experiments on connected community networks with a poisson degree distribution."""
+    print('Running poisson connected community experiements')
     start_time = time.time()
     N_comm = 10  # agents per community
     num_comms = 50  # number of communities
     num_trials = 1000
     rand = np.random.default_rng(420)
-    lam = 5  # this is lambda for both the inner and outer degree distributions
+    lam = 8  # this is lambda for both the inner and outer degree distributions
     configuration = PoissonConfiguration(f'Poisson: lam={lam}, {num_comms} comms, {N_comm} big',
                                          rand, lam, lam, num_comms, N_comm)
     disease = Disease(4, .2)
 
-    with Pool(3) as p:
-        results = p.map(run_poisson_trial, [(configuration, disease, rand)
-                                            for _ in range(num_trials)],
-                        num_trials//3)
+    results = [run_connected_community_trial((configuration, disease, rand))
+               for _ in tqdm(range(num_trials))]
+
+    # with Pool(3) as p:
+    #     results = p.map(run_poisson_trial, [(configuration, disease, rand)
+    #                                         for _ in range(num_trials)],
+    #                     num_trials//3)
+    trial_to_flickering_edges, trial_to_avg_sus = zip(*results)
+    experiment_results = MassDiseaseTestingResult(configuration.name, trial_to_avg_sus,
+                                                  trial_to_flickering_edges)
+    experiment_results.save('results')
+    print(f'Finished ({time.time()-start_time} s).')
+
+
+def uniform_entry_point():
+    """Run experiments on connected community networks with a unifrom degree distribution."""
+    print('Running uniform connected community experiements')
+    start_time = time.time()
+    N_comm = 10  # agents per community
+    num_comms = 50  # number of communities
+    num_trials = 1000
+    rand = np.random.default_rng(0)
+    inner_bounds = (1, 9)
+    outer_bounds = (1, 4)
+    configuration = UniformConfiguration(rand, inner_bounds, outer_bounds, N_comm, num_comms)
+    disease = Disease(4, .2)
+
+    results = [run_connected_community_trial((configuration, disease, rand))
+               for _ in tqdm(range(num_trials))]
+
     trial_to_flickering_edges, trial_to_avg_sus = zip(*results)
     experiment_results = MassDiseaseTestingResult(configuration.name, trial_to_avg_sus,
                                                   trial_to_flickering_edges)
@@ -169,13 +264,14 @@ def poisson_entry_point():
 
 
 def agent_generated_entry_point():
+    print('Running agent generated experiments')
     start_time = time.time()
     num_trials = 1000
     rand = np.random.default_rng(1337)
     N = 500
     lb_connection = 4
     ub_connection = 6
-    steps_to_stability = 10
+    steps_to_stability = 20
     agent_behavior = abg.TimeBasedBehavior(N, lb_connection, ub_connection,
                                            steps_to_stability, rand)
     disease = Disease(4, .2)
@@ -189,12 +285,40 @@ def agent_generated_entry_point():
                                                   f'-{steps_to_stability}',
                                                   trial_to_avg_sus, trial_to_flickering_edges)
     experiment_results.save('results')
-    print(f'Finished ({time.time()-start_time} s).')
+    print(f'Finished experiments with agent generated networks ({time.time()-start_time} s).')
+
+
+def social_circles_entry_point():
+    print('Running social circles experiments.')
+    start_time = time.time()
+    num_trials = 1000
+    rand = np.random.default_rng(0xdeadbeef)
+    N = 500
+    N_purple = int(N * .1)
+    N_blue = int(N * .2)
+    N_green = N - N_purple - N_blue
+    agents = {Agent('green', 30): N_green,
+              Agent('blue', 40): N_blue,
+              Agent('purple', 50): N_purple}
+    grid_dim = (int(N/.003), int(N/.003))  # the denominator is the desired density
+    disease = Disease(4, .2)
+
+    with Pool(5) as p:
+        results = p.map(run_social_circles_trial, [(agents, grid_dim, disease, rand)
+                                                   for _ in range(num_trials)],
+                        num_trials//5)
+    trial_to_flickering_edges, trial_to_avg_sus = zip(*results)
+    experiment_results = MassDiseaseTestingResult(f'Social Circles (Elitist) {grid_dim} {N}',
+                                                  trial_to_avg_sus, trial_to_flickering_edges)
+    experiment_results.save('results')
+    print(f'Finished experiments with elitist networks ({time.time()-start_time} s).')
 
 
 def main():
     # poisson_entry_point()
-    agent_generated_entry_point()
+    uniform_entry_point()
+    # agent_generated_entry_point()
+    # social_circles_entry_point()
 
 
 if __name__ == '__main__':
