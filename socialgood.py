70ae865cec539a3575a1835b612eb1cffbe37a5b
Alexander Anthon <alex.r.anthon@gmail.com>
2021-07-09 11:52:24 -0600

    Added a function for showing the social good each node is getting.

--- socialgood.py
+++ socialgood.py
@@ -1,25 +1,52 @@
 from customtypes import Number
-from typing import Callable
+from typing import Callable, Sequence
 import numpy as np
 import networkx as nx
 import fileio as fio
-import os
+import matplotlib.pyplot as plt
 from tqdm import tqdm
 import csv
+from analyzer import visualize_network
+TDecayFunc = Callable[[int], Number]
 
 
-def main():
-    networks = ('cavemen-50-10', 'elitist-500', 'agent-generated-500',
-                'annealed-agent-generated-500', 'barabasi-albert-500-3', 'cgg-500',
-                'connected-comm-50-10', 'spatial-network', 'watts-strogatz-500-4-.1')
-    network_paths = ['networks/'+name+'.txt' for name in networks]
-    no_errors = True
-    for path in network_paths:
-        if not os.path.exists(path):
-            print(path, 'does not exist.')
-            no_errors = False
-    if not no_errors:
-        return
+class DecayFunction:
+    function_desc = '1/(distance^k)'
+
+    def __init__(self, k: Number):
+        """Return a social good value based on how far away two nodes are."""
+        self.k = k
+
+    def __call__(self, distance: int) -> float:
+        return 1/(distance**self.k)
+
+
+def rate_social_good(G: nx.Graph, decay_func: TDecayFunc) -> float:
+    """
+    Rate a network on how much social good it has.
+    """
+    dist_matrix = nx.floyd_warshall_numpy(G)
+
+    def calc_score(u: int, v: int) -> float:
+        return 0 if u == v else decay_func(dist_matrix[u][v])
+
+    social_good_scores = tuple(tuple(calc_score(u, v) for v in G.nodes) for u in G.nodes)
+    return np.sum(social_good_scores) / (len(G)*(len(G)-1))
+
+
+def node_size_from_social_good(G: nx.Graph, decay_func: TDecayFunc) -> Sequence[Number]:
+    dist_matrix = nx.floyd_warshall_numpy(G)
+
+    def calc_score(u: int, v: int) -> float:
+        return 0 if u == v else decay_func(dist_matrix[u][v])
+
+    social_good_scores = np.array(tuple(tuple(calc_score(u, v) for v in G.nodes) for u in G.nodes))
+    node_size = np.array([np.sum(social_good_scores[u])+np.sum(social_good_scores[:, u])
+                          for u in G.nodes]) / len(G) * 100
+    return node_size
+
+
+def save_social_good_csv(networks: Sequence[str], network_paths: Sequence[str]):
     decay_functions = (DecayFunction(.5), DecayFunction(1), DecayFunction(2))
 
     with open('social-good.csv', 'w') as csvfile:
@@ -35,27 +62,25 @@ def main():
             writer.writerow([name] + scores)
 
 
-class DecayFunction:
-    function_desc = '1/(distance^k)'
+def visualize_social_good(networks: Sequence[str], network_paths: Sequence[str]):
+    for name, path in zip(networks, network_paths):
+        G, layout, _ = fio.read_network(path)
+        node_size = node_size_from_social_good(G, DecayFunction(1))
+        plt.title(f'{name} Node Size')
+        plt.hist(node_size, bins=None)
+        plt.figure()
+        print(f'{name} min = {np.min(node_size):.2f} max = {np.max(node_size):.2f}')
+        visualize_network(G, layout, name, node_size=node_size, block=False)
 
-    def __init__(self, k: Number):
-        self.k = k
-
-    def __call__(self, distance: int) -> float:
-        return 1/(distance**self.k)
+    input('Done.')
 
 
-def rate_social_good(G: nx.Graph, decay_func: Callable[[int], Number]) -> float:
-    """
-    Rate a network on how much social good it has.
-    """
-    dist_matrix = nx.floyd_warshall_numpy(G)
-
-    def calc_score(u: int, v: int) -> float:
-        return 0 if u == v else decay_func(dist_matrix[u][v])
-
-    social_good_scores = tuple(tuple(calc_score(u, v) for v in G.nodes) for u in G.nodes)
-    return np.sum(social_good_scores) / (len(G)*(len(G)-1))
+def main():
+    networks = ('cavemen-50-10', 'elitist-500', 'agent-generated-500',
+                'annealed-agent-generated-500', 'barabasi-albert-500-3', 'cgg-500',
+                'connected-comm-50-10', 'spatial-network', 'watts-strogatz-500-4-.1')
+    network_paths = fio.network_names_to_paths(networks)
+    visualize_social_good(networks, network_paths)
 
 
 if __name__ == '__main__':
