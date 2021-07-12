ddd3ca05ec85d329df8fbde9b62af631b62e6f6b
Alexander Anthon <alex.r.anthon@gmail.com>
2021-07-12 16:13:14 -0600

    Can return None now.

--- experiment/agent_generated.py
+++ experiment/agent_generated.py
@@ -1,19 +1,18 @@
-import networkgen
-from experiment.common import safe_run_trials
+from common import safe_run_trials
 from networkgen import TimeBasedBehavior, AgentBehavior, make_agent_generated_network
 import time
 import numpy as np
 from sim_dynamic import Disease, FlickerBehavior, simulate, make_starting_sir
-from typing import Tuple, Any
+from typing import Optional, Tuple, Any
 import partitioning
 import networkx as nx
+from socialgood import rate_social_good
 
 
-def agent_generated_entry_point():
+def agent_generated_entry_point(seed=1337):
     print('Running agent generated experiments')
-    start_time = time.time()
-    num_trials = 1000
-    rand = np.random.default_rng(1337)
+    num_trials = 100
+    rand = np.random.default_rng(seed)
     N = 500
     lb_connection = 4
     ub_connection = 6
@@ -25,10 +24,9 @@ def agent_generated_entry_point():
     safe_run_trials(f'Agentbased {N}-{lb_connection}-{ub_connection}-{steps_to_stability}',
                     run_agent_generated_trial, (disease, agent_behavior, N, rand), num_trials)
 
-    print(f'Finished experiments with agent generated networks ({time.time()-start_time} s).')
 
-
-def run_agent_generated_trial(args: Tuple[Disease, AgentBehavior, int, Any]) -> Tuple[float, float]:
+def run_agent_generated_trial(args: Tuple[Disease, AgentBehavior, int, Any])\
+        -> Optional[Tuple[float, float, float]]:
     """
     args: (disease to use in the simulation,
            the behavior agents have when generating the network,
@@ -38,17 +36,22 @@ def run_agent_generated_trial(args: Tuple[Disease, AgentBehavior, int, Any]) -> 
     disease, agent_behavior, N, rand = args
     sim_len = 200
     sims_per_trial = 150
-    G = None
-    while G is None:
-        G = make_agent_generated_network(N, agent_behavior)
+    G = make_agent_generated_network(N, agent_behavior)
+    if G is None:
+        return None
 
     to_flicker = partitioning.fluidc_partition(G, 50)
     proportion_flickering = len(to_flicker) / len(G.edges)
+    social_good = rate_social_good(G)
     M = nx.to_numpy_array(G)
 
     network_behavior = FlickerBehavior(M, to_flicker, (True, False), "Probs don't change this")
     avg_sus = np.mean([np.sum(simulate(M, make_starting_sir(len(M), 1),
                                        disease, network_behavior, sim_len, None, rand)[-1][0] > 0)
-                       for _ in range(sims_per_trial)])
+                       for _ in range(sims_per_trial)]) / len(M)
 
-    return proportion_flickering, avg_sus
+    return proportion_flickering, avg_sus, social_good
+
+
+if __name__ == '__main__':
+    agent_generated_entry_point(2)
